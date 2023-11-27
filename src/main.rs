ddc1daada3cb74e7e26bb4a05a95eef792153156
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2023-11-27 10:17:10 +0100

    Assignment 4

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use clap::Parser;
 use walkdir::WalkDir;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Options {
+    #[arg(long, default_value_t = 1)]
+    min_depth: usize,
+
+    #[arg(long, default_value_t = 1)]
+    max_depth: usize,
+}
+
 fn main() {
-    for entry in WalkDir::new(".").min_depth(1).max_depth(1) {
+    let options = Options::parse();
+
+    for entry in WalkDir::new(".")
+        .min_depth(options.min_depth)
+        .max_depth(options.max_depth)
+    {
         println!("{}", entry.unwrap().path().display());
     }
 }
