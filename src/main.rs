5e6859f13d8ff356f0a5bf6fb14f4201b31338b7
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2023-11-27 10:02:07 +0100

    Assignment 3

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use walkdir::WalkDir;
 
 fn main() {
-    for entry in WalkDir::new(".") {
+    for entry in WalkDir::new(".").min_depth(1).max_depth(1) {
         println!("{}", entry.unwrap().path().display());
     }
 }
